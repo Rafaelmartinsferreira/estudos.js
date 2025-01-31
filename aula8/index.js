0f0b57be452d78deb5014b01b63e31f2f1a931ff
Rafael <rafaelmartinsferreira03@gmail.com>
2025-01-31 16:25:12 -0300

    estudando

--- aula8/index.js
+++ aula8/index.js
@@ -6,10 +6,13 @@ const altura = 1.80;
 let imc;    //peso / (altura * altura)
 let anoNascimento;
 
+
 imc = peso / (altura * altura);      
-anoNascimento = 2024 - idade;
+anoNascimento = 2025 - idade;
 
 
-console.log(nomeCompleto, 'tem', idade, 'anos, pesa', peso, 'kg');
+/*console.log(nomeCompleto, 'tem', idade, 'anos, pesa', peso, 'kg');*/
+console.log(`${nomeCompleto} tem ${idade} anos, pesa ${peso} kg`);
+
 console.log('tem', altura, 'de altura e seu IMC é de', imc.toFixed(2));
 console.log(nomeCompleto, 'nasceu em', anoNascimento);
---
+++ aula9/index.js
@@ -0,0 +1,22 @@
+// String, number, undefined, null, boolean, symbol
+
+const nome = 'Rafael'; // string
+const nome1 = "Rafael"; // string
+const nome2 = `Rafael`; // string
+const num = 10; // number
+const num1 = 10.52; //number
+let nomeAluno; // undefined = não aponta para local nenhum na memória
+let sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória
+const aprovado = true; // Boolean = true, false (lógico)
+
+
+//console.log(typeof aprovado, aprovado);
+
+let a = 2;
+const b = a;
+
+console.log(a, b);
+
+a = 3;
+console.log(a, b);
+
